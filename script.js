57a2a9a296fc11be8213bb53726c9cd5d777a7b4
Daksh Prajapati <77955323+DakshPrajapati872@users.noreply.github.com>
2023-05-07 23:44:16 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+const buttons = document.querySelectorAll(".numbers");
+const screen = document.querySelector(".screen");
+const equals = document.querySelector("#btn-equals");
+const clear = document.querySelector("#btn-clear");
+buttons.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    screen.value += e.target.innerHTML;
+  });
+});
+
+equals.addEventListener("click", (e) => {
+  if (!screen.value) {
+    screen.value = "Enter Value";
+  } else {
+    screen.value = eval(screen.value);
+  }
+});
+
+clear.addEventListener("click", (e) => {
+  screen.value = "";
+});
